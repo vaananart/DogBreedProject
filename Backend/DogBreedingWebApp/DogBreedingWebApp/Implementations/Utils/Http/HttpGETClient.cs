3464e5336a0041cc03420ce792632ded2867347f
vaananart <vaananart@gmail.com>
2021-05-19 06:11:15 +1000

    BACKEND: wired up httpclient to service and setup httpclient factory

--- Backend/DogBreedingWebApp/DogBreedingWebApp/Implementations/Utils/Http/HttpGETClient.cs
+++ Backend/DogBreedingWebApp/DogBreedingWebApp/Implementations/Utils/Http/HttpGETClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 using DogBreedingWebApp.Interfaces.Utils.Http;
@@ -9,5 +10,11 @@ namespace DogBreedingWebApp.Implementations.Utils.Http
 {
 	public class HttpGETClient: IHttpGETClient
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public HttpGETClient(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 	}
 }
