22a4ab97194f1052da4b292e09ec052fc024aca2
vaananart <vaananart@gmail.com>
2021-05-19 04:37:53 +1000

    PROJECT: inital commit with basic project structure

---
+++ Backend/DogBreedingWebApp/DogBreedingWebApp/Controllers/Dogs/BreedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogBreedingWebApp.Controllers.Dogs
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class BreedController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get(string breedName)
+		{
+			return null;
+		}
+	}
+}
---
+++ Backend/DogBreedingWebApp/DogBreedingWebApp/Controllers/Dogs/SubBreedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogBreedingWebApp.Controllers.Dogs
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class SubBreedController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return null;
+		}
+	}
+}
